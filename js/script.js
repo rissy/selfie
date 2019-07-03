d0f392a043539dd82593e88ce24df1a0829460be
Aleksey Goroshevskiy <ag@rissik.ru>
2019-07-03 20:45:42 +0300

    reindent for 4 spaces

--- js/script.js
+++ js/script.js
@@ -1,70 +1,70 @@
 ;(function () {
-  window.onpopstate = loadState;
+    window.onpopstate = loadState;
 
-  var menu = document.getElementById('menu');
+    var menu = document.getElementById('menu');
 
-  menu.classList.toggle('no-display');
+    menu.classList.toggle('no-display');
 
-  var PAGE_SUFFIX = '-page';
-  var TAG_PREFIX = '/#/';
+    var PAGE_SUFFIX = '-page';
+    var TAG_PREFIX = '/#/';
 
-  var elementsIds = ['resume', 'portfolio', 'about'];
-  var elements = {};
-  
-  elementsIds.forEach(function(id) {
-    elements[id] = {
-      tab: document.getElementById(id),
-      page: document.getElementById(id + PAGE_SUFFIX)
-    }
-  })
+    var elementsIds = ['resume', 'portfolio', 'about'];
+    var elements = {};
 
-  var previewElementId = elementsIds[0];
+    elementsIds.forEach(function(id) {
+        elements[id] = {
+            tab: document.getElementById(id),
+            page: document.getElementById(id + PAGE_SUFFIX)
+        }
+    });
 
-  loadState();
+    var previewElementId = elementsIds[0];
 
-  menu.onclick = function(ev) {
-    var element = ev.target;
+    loadState();
 
-    while (element !== this) {
-      if (element.tagName === 'DIV') {
-        onMenuClick(element.id);
-        addUrlHash(element.id);
+    menu.onclick = function(ev) {
+        var element = ev.target;
 
-        previewElementId = element.id;
+        while (element !== this) {
+            if (element.tagName === 'DIV') {
+                onMenuClick(element.id);
+                addUrlHash(element.id);
 
-        return;
-      }
+                previewElementId = element.id;
 
-      element = element.parentNode;
-    }
-  };
+                return;
+            }
 
-  function onMenuClick(elementId) {
-    if (elementId === previewElementId) {
-      return;
-    }
+            element = element.parentNode;
+        }
+    };
 
-    elements[elementId].tab.classList.toggle('active-menu-item');
-    elements[previewElementId].tab.classList.toggle('active-menu-item');
+    function onMenuClick(elementId) {
+        if (elementId === previewElementId) {
+            return;
+        }
 
-    elements[elementId].page.classList.toggle('no-display');
-    elements[previewElementId].page.classList.toggle('no-display');
-  }
+        elements[elementId].tab.classList.toggle('active-menu-item');
+        elements[previewElementId].tab.classList.toggle('active-menu-item');
 
-  function loadState() {
-    var urlHash = window.location.hash.substr(2);
-    var elementId = elementsIds.indexOf(urlHash) !== -1 ? urlHash : elementsIds[0];
-
-    onMenuClick(elementId);
-  
-    previewElementId = elementId;
-  }
-
-  function addUrlHash(elementId) {
-    if (elementId === previewElementId) {
-      return;
+        elements[elementId].page.classList.toggle('no-display');
+        elements[previewElementId].page.classList.toggle('no-display');
     }
 
-    history.pushState({}, '' ,TAG_PREFIX + elementId);
-  }
+    function loadState() {
+        var urlHash = window.location.hash.substr(2);
+        var elementId = elementsIds.indexOf(urlHash) !== -1 ? urlHash : elementsIds[0];
+
+        onMenuClick(elementId);
+
+        previewElementId = elementId;
+    }
+
+    function addUrlHash(elementId) {
+        if (elementId === previewElementId) {
+            return;
+        }
+
+        history.pushState({}, '' ,TAG_PREFIX + elementId);
+    }
 })();
